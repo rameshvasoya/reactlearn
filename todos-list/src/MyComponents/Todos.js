31aa72220d12b477dfb6988eb94730ca5198537e
rameshsit <55971103+rameshsit@users.noreply.github.com>
2021-12-16 01:10:22 +0530

    todos list

--- todos-list/src/MyComponents/Todos.js
+++ todos-list/src/MyComponents/Todos.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import TodoItem from './MyComponents/TodoItem'
+import TodoItem from '../MyComponents/TodoItem'
 
 function Todos(props) {
     return (
         <div className="container">
         <h2>Todos List</h2>
         {/* {props.Todos} */}
-        <TodoItem todo={Todos}/>
+
+        {props.todos.map((todo) => {
+            return <TodoItem todo={todo}/>
+        })}
+        
         </div>
     )
 }
