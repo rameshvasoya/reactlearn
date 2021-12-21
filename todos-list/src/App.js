1a54f41772169e87bda84c20a934d9e06ab25730
rameshsit <55971103+rameshsit@users.noreply.github.com>
2021-12-21 18:02:11 +0530

    local storage

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./MyComponents/Header";
 import Footer from "./MyComponents/Footer";
 import Todos from "./MyComponents/Todos";
-import {AddTodo} from "./MyComponents/AddTodo";
-
+import { AddTodo } from "./MyComponents/AddTodo";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos")=== null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
   const onDelete = (todo) => {
     console.log(" I am OnDelete of todo", todo);
 
@@ -15,38 +20,32 @@ function App() {
         return e !== todo;
       })
     );
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  const addTodo = (title, desc)=>{
+  const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc);
-    let sno = todos[todos.length-1].sno + 1;
-    const myTodo = {
-      sno:sno,
-      title:title,
-      desc:desc,
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
     }
-    // add new todo 
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    // add new todo
     setTodos([...todos, myTodo]);
     console.log(myTodo);
-  }
-  
-  const [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: "Go to the market",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-    {
-      sno: 2,
-      title: "Go to the mall",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-    {
-      sno: 3,
-      title: "Go to the work",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    },
-  ]);
+  };
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
       {/* <Header title="MyTodosList" SearchBar={false}/> */}
--- todos-list/src/MyComponents/AddTodo.js
+++ todos-list/src/MyComponents/AddTodo.js
@@ -1,33 +1,63 @@
 import React, { useState } from "react";
 
 export const AddTodo = (props) => {
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const submit = (e) => {
-        e.preventDefault();
-        if(!title || !desc ){
-            alert("Title or Description cannot Blank");
-        }
-        props.addTodo(title, desc);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!title || !desc) {
+      alert("Title or Description cannot Blank");
+    } else {
+      props.addTodo(title, desc);
+      setTitle("");
+      setDesc("");
     }
-    return (
-        <div className='container'>
-            <div className="card text-dark bg-light mb-3 mt-3">
-            <div className="card-header"><h2>Add a Todo</h2></div>
-                <div className="card-body">
-                    <form onSubmit={submit}>
-                        <div className="mb-3">
-                            <label htmlFor="title" className="form-label">Todo Title</label>
-                            <input type="text" className="form-control" value={title} onChange={(e)=>{setTitle(e.target.value)}} id="text" aria-describedby="emailHelp"/>
-                        </div>
-                        <div className="mb-3">
-                            <label JSX ="desc" className="form-label">Todo Description</label>
-                            <input type="text" className="form-control" value={desc} onChange={(e)=>{setDesc(e.target.value)}} id="desc"/>
-                        </div>
-                        <button type="submit" className="btn btn-sm btn-primary"  >Add Todo</button>
-                    </form>
-                </div>
-            </div>
+  };
+
+  return (
+    <div className="container">
+      <div className="card text-dark bg-light mb-3 mt-3">
+        <div className="card-header">
+          <h2>Add a Todo</h2>
         </div>
-    )
-}
+        <div className="card-body">
+          <form onSubmit={submit}>
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Todo Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+                id="text"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label JSX="desc" className="form-label">
+                Todo Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value={desc}
+                onChange={(e) => {
+                  setDesc(e.target.value);
+                }}
+                id="desc"
+              />
+            </div>
+            <button type="submit" className="btn btn-sm btn-primary">
+              Add Todo
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
